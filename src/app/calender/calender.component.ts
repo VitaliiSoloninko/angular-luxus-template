9f60e0108182cbc87df98b43a1f31370bc8ffd88
22051988Di <vitalii.soloninko@gmail.com>
2025-02-13 11:42:22 +0100

    0.3	Logic: today, firstDay, weekDays, daysOfMonth

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { DateTime, Info, Interval } from 'luxon';
 
 @Component({
   selector: 'app-calender',
   imports: [],
   templateUrl: './calender.component.html',
-  styleUrl: './calender.component.scss'
+  styleUrl: './calender.component.scss',
 })
 export class CalenderComponent {
+  today: Signal<DateTime> = signal(DateTime.local());
+  firstDayOfActiveMonth: WritableSignal<DateTime> = signal(
+    this.today().startOf('month')
+  );
+  weekDays: Signal<string[]> = signal(Info.weekdays('short'));
+  daysOfMonth: Signal<DateTime[]> = computed(() => {
+    return Interval.fromDateTimes(
+      this.firstDayOfActiveMonth().startOf('month'),
+      this.firstDayOfActiveMonth().endOf('month').endOf('week')
+    )
+      .splitBy({ days: 1 })
+      .map((day) => {
+        if (day.start === null) {
+          throw new Error('Wrong day');
+        }
+        return day.start;
+      });
+  });
 
+  constructor() {
+    console.log(this.daysOfMonth());
+  }
 }
